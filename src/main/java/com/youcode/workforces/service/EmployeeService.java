8eb0a1197566afbb4f7618add70ca4cded5f2431
khalid fifel <fifle.khalid99@gmail.com>
2023-10-31 15:50:13 +0100

    add EmployeeService and sont bean

---
+++ src/main/java/com/youcode/workforces/service/EmployeeService.java
@@ -0,0 +1,26 @@
+package com.youcode.workforces.service;
+
+import com.youcode.workforces.entities.Employee;
+
+import java.util.List;
+
+public class EmployeeService {
+    private final Employee employee;
+
+    public EmployeeService(Employee employee) {
+        this.employee = employee;
+    }
+
+    public List<Employee> findAll() {
+        return null;
+    }
+
+    public Employee save(Employee employee) {
+        return null;
+    }
+
+    public Employee update(Employee employee) {
+        return null;
+    }
+
+}
